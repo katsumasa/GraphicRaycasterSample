a42c87015324a6f100fb8f2bdf449619f32f7a73
katsumasa <katsumasa@unity3d.com>
2021-03-10 14:03:48 +0900

    Add files via upload

---
+++ Assets/Scripts/CheckEventSystem.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CheckEventSystem : MonoBehaviour
+{
+    public EventSystem mEventSystem;
+    PointerEventData mPointerEventData;
+    List<RaycastResult> mRaycastResults;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mPointerEventData = new PointerEventData(mEventSystem);
+        mRaycastResults = new List<RaycastResult>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            mRaycastResults.Clear();
+            mPointerEventData.position = Input.mousePosition;
+            mEventSystem.RaycastAll(mPointerEventData, mRaycastResults);
+            foreach(var raycastResult in mRaycastResults)
+            {
+                Debug.Log(raycastResult);
+            }
+        }
+    }
+}
